436d246ead10af054e486e399136b87d1c9efb74
Syahreza-Ferdian <syahrezafistiferdian32@gmail.com>
2024-12-15 01:07:31 +0700

    `feat`: add Car Routes

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Api\RegisterController;
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\CarController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -20,10 +21,24 @@ use Illuminate\Support\Facades\Route;
 //     return $request->user();
 // });
 
+Route::get("/stuff", function() {
+    return ["a" => 1];
+});
+
 Route::prefix('/user')->group(function () {
     Route::post('/register', RegisterController::class)->name('register');
     Route::post('/login', [AuthController::class, 'login'])->name('login');
     Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth.jwt');
     Route::post('/refresh', [AuthController::class, 'refresh'])->middleware('auth.jwt');
     Route::get('/current-user', [AuthController::class, 'me'])->middleware('auth.jwt');
+});
+
+Route::prefix('/car')->group(function() {
+    Route::get('/all', [CarController::class, 'getAll'])->middleware('auth.jwt');
+    Route::get('/{id}', [CarController::class, 'getByID'])->middleware('auth.jwt');
+    Route::get('/category/{id_kategori}', [CarController::class, 'getCarsByCategory'])->middleware('auth.jwt');
+    Route::get('/status/{id_status}', [CarController::class, 'getCarsByStatusID'])->middleware('auth.jwt');
+    Route::post('/add', [CarController::class, 'addNew'])->middleware('auth.jwt')->middleware('role:admin');
+    Route::patch('/update/{id}', [CarController::class, 'update'])->middleware('auth.jwt')->middleware('role:admin');
+    Route::delete('/delete/{id}', [CarController::class, 'deleteCar'])->middleware('auth.jwt')->middleware('role:admin');
 });
